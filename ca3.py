36fa21737ec3a4296ce7424acb9de0d0c56a26f3
Taha Shabani <taha.shabani.m@gmail.com>
2020-04-30 01:20:55 +0430

    read intial data done

--- ca3.py
+++ ca3.py
@@ -1,11 +1,13 @@
 import nltk
+import csv
+import pandas as pd
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 
 dataFile = "data.csv"
-test = "test.csv"
+testFile = "test.csv"
 
 def readFile(fileName) :
     file = open(fileName, 'r+')
@@ -41,3 +43,68 @@ def preprocessData(context) :
     wordList = lemmatizeWords(wordList)
     return wordList
     
+    
+class Classifier :
+    def __init__(self, dataFile, testFile) :
+        [self.travelTrainData, self.travelEvaluateData, self.businessTrainData, self.businessEvaluateData, self.styleTrainData, self.styleEvaluateData] = self.getDatas(dataFile)
+        # print(len(self.travelTrainData))
+        # print(len(self.travelEvaluateData))
+        
+        # print(len(self.businessTrainData))
+        # print(len(self.businessEvaluateData))
+        
+        # print(len(self.styleTrainData))
+        # print(len(self.styleEvaluateData))
+    
+    def getDatas(self, dataFile) : 
+        col_list = ["index", "category", "headline", "short_description"]
+        df = pd.read_csv(dataFile, usecols=col_list)
+        travelData = []
+        businessData = []
+        styleData = []
+        for index, dfRow in df.iterrows() : 
+            if dfRow[1] == "TRAVEL" : 
+                travelData.append(dfRow)
+            elif dfRow[1] == "BUSINESS" : 
+                businessData.append(dfRow)
+            elif dfRow[1] == "STYLE & BEAUTY" : 
+                styleData.append(dfRow)
+        
+        travelSize = len(travelData)
+        businessSize = len(businessData)
+        styleSize = len(styleData)
+        
+        # print(len(travelData))
+        # print(len(businessData))
+        # print(len(styleData))
+        
+        # print("**************************")
+        
+        travelTrainData = []
+        businessTrainData = []
+        styleTrainData = []
+        travelEvaluateData = []
+        buisinessEvaluateData = []
+        styleEvaluateData = []
+        
+        for i in range(travelSize) :
+            if (i <= (int)(travelSize * 0.8)) :
+                travelTrainData.append(travelData[i])
+            else:
+                travelEvaluateData.append(travelData[i])
+        for i in range(businessSize) :
+            if (i <= (int)(businessSize * 0.8)) :
+                businessTrainData.append(businessData[i])
+            else:
+                buisinessEvaluateData.append(businessData[i])
+        for i in range(styleSize) :
+            if (i <= (int)(styleSize * 0.8)) :
+                styleTrainData.append(styleData[i])
+            else:
+                styleEvaluateData.append(styleData[i])
+                
+        return [travelTrainData, travelEvaluateData, businessTrainData, buisinessEvaluateData, styleTrainData, styleEvaluateData]
+            
+        
+        
+cl = Classifier(dataFile, testFile)       
